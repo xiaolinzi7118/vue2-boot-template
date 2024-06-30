bea79daf8c5c0dabec9df9d21382d641ff3f3c4d
xiaolinzi7118 <1106147743@qq.com>
2024-06-30 18:17:02 +0800

    修改路径

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: '/vue2-boot-template',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
